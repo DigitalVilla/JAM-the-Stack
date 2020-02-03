d2ff5f98365e7cb671f9630745eb6f5a18049c73
Digital Villa <digitalvilla01@gmail.com>
2020-02-02 19:12:51 -0700

    Contact | Add react form

---
+++ sections/contact/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../components/form.jsx'
+import './style.css'
+
+export default () => (
+	<>
+		<main>
+			<h1>Send a Message</h1>
+
+			<Form />
+		</main>
+	</>
+);
