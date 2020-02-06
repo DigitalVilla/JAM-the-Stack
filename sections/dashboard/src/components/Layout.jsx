69d9bb82f49eb1b72e7dea292a3337536887044b
Digital Villa <digitalvilla01@gmail.com>
2020-02-05 22:09:52 -0700

    Auth created

--- sections/dashboard/src/components/Layout.jsx
+++ sections/dashboard/src/components/Layout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from "gatsby";
+import { IdentityContextProvider } from "react-netlify-identity-widget";
 import './layout.css';
 
 const Layout = ({children}) => {
 	return (
-		<>
+		<IdentityContextProvider url="https://jamthestack-auth.netlify.com">
 			<header>
 				<Link to="/">JAMstack App</Link>
 			</header>
 			<main>{children}</main>
-		</>
+		</IdentityContextProvider>
 	)
 }
 
